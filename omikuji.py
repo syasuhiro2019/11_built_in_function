00fdfac32f84ad0f7f47e20b3ed1021ae1d4da9d
Sato <s_ychm@yahoo.co.jp>
2019-03-10 09:40:53 +0900

    末吉追加

--- omikuji.py
+++ omikuji.py
@@ -1,8 +1,8 @@
 import random
 
-omikuji = ['大吉', '吉', '凶']
+omikuji = ['大吉', '吉', '凶', '末吉']
 
-print(omikuji[random.randint0, 2)])
+print(omikuji[random.randint0, 3])
 
 #  または
 # idx=random.randint(0, 2)
