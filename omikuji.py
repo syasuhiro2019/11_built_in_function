9fd3ea12d189f5a5043bf87de2acefb0168b58b5
Sato <s_ychm@yahoo.co.jp>
2019-03-10 10:00:20 +0900

    random.choice追加

--- omikuji.py
+++ omikuji.py
@@ -1,9 +1,11 @@
 import random
 
-omikuji = ['大吉', '吉', '凶', '末吉', '大凶','中吉']
+omikuji = ['大吉', '吉', '凶', '末吉', '大凶', '中吉']
 
 # print(omikuji[random.randint(0, 4)]
 
-idx=random.randint(0, len(omikuji)-1)
+# idx = random.randint(0, len(omikuji) - 1)
+#
+# print(omikuji[idx])
 
-print(omikuji[idx])
+print(random.choice(omikuji))
